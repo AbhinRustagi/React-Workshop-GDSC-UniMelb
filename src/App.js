e7d24e04dab8f80934a7b511c5b972cf6d5f8592
AbhinRustagi <rustagi.abhin@gmail.com>
2024-04-08 21:45:17 +1000

    feat: add final styles

--- src/App.js
+++ src/App.js
@@ -6,11 +6,29 @@ export default function App() {
       <div className="top">
         <div className="top-title">hello.</div>
       </div>
+      <div className="console">
+        <div className="note form">
+          <input
+            type="text"
+            name=""
+            id=""
+            className="note-title form-title"
+            placeholder="Title goes here"
+          />
+          <textarea
+            name=""
+            id=""
+            placeholder="Add your notes here"
+            className="note-body form-body"
+          ></textarea>
+          <button>Add</button>
+        </div>
+      </div>
       <div className="notes-grid">
         <div className="note">
           <div className="note-title">foo</div>
           <div className="note-body">bar</div>
-          <button className="delete-btn">delete</button>
+          <button>Delete</button>
         </div>
       </div>
     </div>
