51f3ea1514c085979d01f5e79510ace4137e4a93
AbhinRustagi <rustagi.abhin@gmail.com>
2024-04-08 17:49:56 +1000

    feat: add styles

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="top">
+        <div className="top-title">hello.</div>
+      </div>
+      <div className="notes-grid">
+        <div className="note">
+          <div className="note-title">foo</div>
+          <div className="note-body">bar</div>
+          <button className="delete-btn">delete</button>
+        </div>
+      </div>
     </div>
   );
 }
