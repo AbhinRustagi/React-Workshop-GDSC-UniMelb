d6277aaa2d37fb8b5559491e4e06fd1b952be4b6
AbhinRustagi <rustagi.abhin@gmail.com>
2024-04-11 12:46:51 +1000

    Revert "feat: add logic"
    This reverts commit 28d9fc192beb009312e224238aeb363c655da4c0.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,6 @@
-import { useState, useEffect } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [notes, setNotes] = useState([]);
-  const [noteCounter, setNoteCounter] = useState(0);
-  const [noteTitle, setNoteTitle] = useState("");
-  const [noteBody, setNoteBody] = useState("");
-
-  useEffect(() => {
-    setNoteTitle("");
-    setNoteBody("");
-  }, [notes]);
-
-  const addNote = () => {
-    const id = noteCounter + 1;
-    const note = {
-      id,
-      title: noteTitle,
-      body: noteBody,
-    };
-
-    setNotes((notes) => [note, ...notes]);
-    setNoteCounter(id);
-  };
-
-  const deleteNote = (id) => {
-    const index = notes.findIndex((note) => note.id === id);
-    const tempNotes =
-      index > -1 ? notes.filter((note) => note.id !== id) : notes;
-    setNotes(tempNotes);
-  };
-
   return (
     <div className="App">
       <div className="top">
@@ -42,8 +12,6 @@ export default function App() {
             type="text"
             name=""
             id=""
-            value={noteTitle}
-            onChange={(e) => setNoteTitle(e.target.value)}
             className="note-title form-title"
             placeholder="Title goes here"
           />
@@ -52,20 +20,16 @@ export default function App() {
             id=""
             placeholder="Add your notes here"
             className="note-body form-body"
-            value={noteBody}
-            onChange={(e) => setNoteBody(e.target.value)}
           ></textarea>
-          <button onClick={addNote}>Add</button>
+          <button>Add</button>
         </div>
       </div>
       <div className="notes-grid">
-        {notes.map((note) => (
-          <div key={note?.id} className="note">
-            <div className="note-title">{note?.title}</div>
-            <div className="note-body">{note?.body}</div>
-            <button onClick={() => deleteNote(note?.id)}>Delete</button>
-          </div>
-        ))}
+        <div className="note">
+          <div className="note-title">foo</div>
+          <div className="note-body">bar</div>
+          <button>Delete</button>
+        </div>
       </div>
     </div>
   );
