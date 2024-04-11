c98b291bc14aca16e2d78481fb65ae3f64f7f585
AbhinRustagi <rustagi.abhin@gmail.com>
2024-04-11 12:47:16 +1000

    revert basic code

--- src/App.js
+++ src/App.js
@@ -1,36 +1,5 @@
 import "./styles.css";
 
 export default function App() {
-  return (
-    <div className="App">
-      <div className="top">
-        <div className="top-title">hello.</div>
-      </div>
-      <div className="console">
-        <div className="note form">
-          <input
-            type="text"
-            name=""
-            id=""
-            className="note-title form-title"
-            placeholder="Title goes here"
-          />
-          <textarea
-            name=""
-            id=""
-            placeholder="Add your notes here"
-            className="note-body form-body"
-          ></textarea>
-          <button>Add</button>
-        </div>
-      </div>
-      <div className="notes-grid">
-        <div className="note">
-          <div className="note-title">foo</div>
-          <div className="note-body">bar</div>
-          <button>Delete</button>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
